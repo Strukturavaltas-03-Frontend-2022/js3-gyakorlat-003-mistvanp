71ff801b47a401677cdb55dc435727a2a7ef9b5b
Istvan888 <marai.istvanp@gmail.com>
2022-08-31 16:49:58 +0200

    Done

--- solutions/app2.js
+++ solutions/app2.js
@@ -1,1 +1,27 @@
-
+const cookieHandler = {
+    getAll() {
+      let cookiesObj = {};
+      const cookies = document.cookie.split(";");
+      cookies.forEach((cookie) => {
+        cookiesObj[cookie.slice(0, cookie.indexOf("=")).trim()] = cookie
+          .slice(cookie.indexOf("=") + 1)
+          .trim();
+      });
+  
+      return cookiesObj;
+    },
+    toSessionStorage() {
+      const cookiesObj = this.getAll();
+      Object.entries(cookiesObj).forEach(([key, value]) => {
+        sessionStorage.setItem(key, value);
+      });
+    },
+  
+    flush() {
+      const cookiesObj = this.getAll();
+      Object.keys(cookiesObj).forEach((key) => {
+        document.cookie = `${key}=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;`;
+      });
+    },
+  };
+  export { cookieHandler };
